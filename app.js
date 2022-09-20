8ff6337644abaf981a90262ddcd42e2d021bd611
ishimweeli <ishimweeli0@gmail.com>
2022-09-20 13:21:41 +0200

    gnf

--- app.js
+++ app.js
@@ -14,12 +14,26 @@ require('./db/db')
 
 
 const app = express()
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//  next();
 app.use(cors())
+ 
+// });
+
+// app.use(cors({
+//     origin: '*'
+// }));
 app.use(express.json())
 app.use(cookieParser());
 app.use(userRouter)
 app.use(amarengaRouter)
 app.use(announcementRouter)
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
