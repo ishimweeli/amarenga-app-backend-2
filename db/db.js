fcdb1ec889cf2944c580ac5bee449c7d1d1fe2b0
ishimweeli <97574791+ishimweeli@users.noreply.github.com>
2023-04-01 18:20:34 +0200

    new db

--- db/db.js
+++ db/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
+    useNewUrlParser: true, useUnifiedTopology: true 
 
 
-}).then(console.log("mongoose db connected"))
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB Atlas: ', err);
+  });
