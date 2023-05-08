2368a533d558356b46362ceeea227b73706005d5
ishimweeli <97574791+ishimweeli@users.noreply.github.com>
2023-05-08 21:48:17 +0200

    new publication router

--- utils/multer.js
+++ utils/multer.js
@@ -3,12 +3,5 @@ const path = require("path");
 // Multer config
 module.exports = multer({
   storage: multer.diskStorage({}),
-  fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-      if (ext !== ".pdf" && ext !== '.gif' && ext !== '.zip') {
-      cb(console.log("File type is not supported"), false);
-      return;
-    }
-    cb(null, true);
-  },
+
 });
